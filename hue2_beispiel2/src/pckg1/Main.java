b7151236d7e30e0e145129bbcff35b0c498fc5eb
abgehtdiepost <david.gruber@rb-werbeagentur.at>
2019-09-24 11:17:45 +0200

    Main

---
+++ hue2_beispiel2/src/pckg1/Main.java
@@ -0,0 +1,30 @@
+package pckg1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author david
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        List<String> liste = new ArrayList<String>();
+        liste.add("i");
+        liste.add("b");
+        HalloJavamitForEach burr = new HalloJavamitForEach(liste);
+        
+    }
+    
+}
